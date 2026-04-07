d7ecf4ec420eeaf8601ade5e3d4860a0a0af7681
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2026-04-07 15:06:15 -0300

    init swift UI app

---
+++ HighLit/HighLit/HighLitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighLitApp.swift
+//  HighLit
+//
+//  Created by Bernardo Cruz Rohlfs on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct HighLitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
