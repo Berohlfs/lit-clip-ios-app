3bd564ea4e4f7acfbbb55a3227a1d5a450a6fdd0
Bernardo Cruz Rohlfs <90404673+Berohlfs@users.noreply.github.com>
2026-04-08 10:25:06 -0300

    init: watch app

---
+++ HighLit/LitClipWatch Watch App/LitClipWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitClipWatchApp.swift
+//  LitClipWatch Watch App
+//
+//  Created by Bernardo Cruz Rohlfs on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LitClipWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
